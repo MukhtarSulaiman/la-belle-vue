e1e68ac29056c89a1cacc7d39f1d3e1adfa9d566
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-11-01 12:51:46 +0100

    Added vue project

---
+++ cli-version/src/components/MenuItem.vue
@@ -0,0 +1,89 @@
+
+<template>
+    <div class="menu-item">
+                <img
+                    class="menu-item__image"
+                    v-bind:src="image.source"
+                    v-bind:alt="image.alt"
+                />
+                <div>
+                    <h3>{{ name }}</h3>
+                    <p>Prix: {{ generatePrice }}</p>
+                    <p v-if="inStock === true">En stock</p>
+                    <p v-else>En rupture de stock</p>
+                    <div>
+                        <label for="add-item-quantity">Quantité : {{ quantity }}</label>
+                        <input v-model.number="quantity" id="add-item-quantity" type="number"/>
+                        <button @click="updateShoppingCart( quantity)">Ajouter au panier d'achat</button>
+                    </div>
+                </div>
+            </div>
+</template>
+
+
+<script>
+    export default {
+        name: "MenuItem",
+        props: {
+            image: {
+                type: Object,
+                required: true
+            },
+            inStock: {
+                type: Boolean,
+                required: true
+            },
+            name: {
+                type: String,
+                requierd: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true
+            }
+        },
+        data() {
+            return {
+                onSale: false
+            }
+        },
+        computed: {
+            generatePrice() {
+                if (this.onSale) {
+                    return (this.price * 0.9).toFixed(2)
+                } else {
+                    return this.price
+                }
+            }
+        },
+        beforeMount() {
+            const tody = new Date().getDate();
+
+            if (tody % 2 === 0) {
+                this.onSale = true
+            }
+        },
+        methods: {
+            updateShoppingCart(amount) {
+                this.$emit('add-items-to-cart', amount)
+            }
+        }
+    }
+</script>
+
+<style>
+
+.menu-item {
+  display: flex;
+  width: 500px;
+  justify-content: space-between;
+  margin-bottom: 30px;
+}
+.menu-item__image {
+  max-width: 300px;
+}
+</style>
